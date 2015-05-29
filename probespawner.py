2446180df5fdbc30c04f7a5c469b0e8c5d72acc8
Filipe de Almeida <filipe@marmelos.com>
2015-05-29 17:30:19 +0100

    License headers where missing

--- probespawner.py
+++ probespawner.py
@@ -1,3 +1,6 @@
+# Any copyright is dedicated to the Public Domain.
+# http://creativecommons.org/publicdomain/zero/1.0/
+
 from __future__ import with_statement
 from pprint import pprint
 #from databaseprobe import DatabaseProbe
